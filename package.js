577991193c1ca76c788cd9b85e6fd80327dcf145
Jakob Rosenberg <jakobrosenberg@gmail.com>
2015-09-15 18:04:02 +0200

    fixed wrong name in test dependencies

--- package.js
+++ package.js
@@ -33,6 +33,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('mithril');
+  api.use('grottolabs:mithril');
   api.addFiles('mithril-tests.js');
 });
